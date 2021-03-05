3b1341058d32582d35831451775fe08fd76a35a1
emnakorbi <57458808+emnakorbi@users.noreply.github.com>
2021-03-05 12:23:11 +0100

    Update SubscriptionController.java

--- src/main/java/tn/dari/spring/controller/SubscriptionController.java
+++ src/main/java/tn/dari/spring/controller/SubscriptionController.java
@@ -3,7 +3,7 @@ package tn.dari.spring.controller;
 import java.util.List;
 
 import org.apache.logging.log4j.Logger;
-import org.junit.jupiter.api.Test;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
