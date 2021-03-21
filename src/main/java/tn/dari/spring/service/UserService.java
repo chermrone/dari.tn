a899318216c4f1d5152f5cb19b8e95279423d115
Emna <emna.korbi1@esprit.tn>
2021-03-21 19:32:34 +0100

    regulation of service user (ban user)

--- src/main/java/tn/dari/spring/service/UserService.java
+++ src/main/java/tn/dari/spring/service/UserService.java
@@ -1,10 +1,15 @@
 package tn.dari.spring.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import tn.dari.spring.entity.Ad;
+import tn.dari.spring.entity.Claim;
 import tn.dari.spring.entity.User;
 
 import tn.dari.spring.exception.UserNotFoundException;
@@ -15,6 +20,9 @@ public class UserService implements UIuser {
 	@Autowired
 	UserRepository ur;
 
+	@Autowired
+	AdService adserv;
+
 	@Override
 	public List<User> GetAllUsers() {
 
@@ -24,7 +32,8 @@ public class UserService implements UIuser {
 	@Override
 	public User GetUserById(Long id) {
 		return ur.findById(id).get();
-		//return ur.findById(id).orElseThrow(() -> new UserNotFoundException("user by id= " + id + " was not found"));
+		// return ur.findById(id).orElseThrow(() -> new
+		// UserNotFoundException("user by id= " + id + " was not found"));
 	}
 
 	@Override
@@ -42,7 +51,7 @@ public class UserService implements UIuser {
 	@Override
 	public User GetUserByUserName(String username) {
 
-		return ur.findByUserName(username);//.get();
+		return ur.findByUserName(username);// .get();
 	}
 
 	@Override
@@ -61,6 +70,26 @@ public class UserService implements UIuser {
 	public void DeleteUser(Long id) {
 		ur.deleteById(id);
 
+	}
+
+	@Override
+	public void BanUser(Long id) {
+		List<Ad> ad = adserv.getAll();
+		List<Ad> aduser = new ArrayList<>();
+		for (Ad ad2 : ad) {
+			if (ad2.getUs().getIdUser() == id) {
+				aduser.add(ad2);
+			}
+		}
+		List<Claim> clmuser = new ArrayList<>();
+		for (Ad ad3 : aduser) {
+			clmuser.addAll(ad3.getClaims());
+		}
+		if (clmuser.size() >= 10) {
+			User us = GetUserById(id);
+			us.setUserState(false);
+			UpdateUser(us);
+		}
 	}
 
 }
