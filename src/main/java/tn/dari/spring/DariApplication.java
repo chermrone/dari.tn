af90ab8f673a143f64ff66840ff37c79eb68609d
Emna <marouane.cherif1@esprit.tn>
2021-03-22 16:17:07 +0100

    trying paypal

--- src/main/java/tn/dari/spring/DariApplication.java
+++ src/main/java/tn/dari/spring/DariApplication.java
@@ -2,7 +2,10 @@ package tn.dari.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @ComponentScan("tn.dari.spring")
@@ -11,5 +14,9 @@ public class DariApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DariApplication.class, args);
 	}
+	@Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
+    }
 
 }
