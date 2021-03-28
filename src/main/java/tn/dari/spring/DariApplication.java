7e5081c18a087f2859d425674ad69e95e9fd76e6
jihen1998 <57370028+jihen1998@users.noreply.github.com>
2021-03-28 19:20:12 +0200

    Revert "Merge branch 'master' into jihen"
    This reverts commit 88228714f7b0bd03fc58c281d1f935210d9654d2, reversing
    changes made to f3dc21c09bb16560857c3721b20518ea7471b792.

--- src/main/java/tn/dari/spring/DariApplication.java
+++ src/main/java/tn/dari/spring/DariApplication.java
@@ -2,7 +2,6 @@ package tn.dari.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
