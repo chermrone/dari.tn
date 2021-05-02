170fee488d290cbd83ebddb4be387f76bd49fa78
seifsalem <seifeddine.salem1@esprit.tn>
2021-05-02 00:28:44 +0000

    fixing

--- src/main/java/tn/dari/spring/entity/City.java
+++ src/main/java/tn/dari/spring/entity/City.java
@@ -1,7 +1,5 @@
 package tn.dari.spring.entity;
 
-import java.util.Date;
-import java.util.Set;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -9,18 +7,11 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-import org.springframework.beans.factory.InitializingBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.test.context.jdbc.Sql;
-
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
-import tn.dari.spring.enumeration.TypeBatiment;
-import tn.dari.spring.enumeration.Typead;
-import tn.dari.spring.repository.Citiesrepository;
 
 @Entity
 @Getter
